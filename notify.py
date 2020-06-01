51d1f3833a741d2ffa3e3f72d99f3a65690a1d21
Lei Shi <me@leishi.io>
2020-06-01 14:46:48 -0700

    Switch from IFTTT to pushbullet.

--- notify.py
+++ notify.py
@@ -34,7 +34,7 @@ def ifttt_send(event, key, title, diffs):
             req = requests.post(url, data=payload)
             print("IFTTT send: {}".format(req.status_code))
         except Exception as e:
-            print(e) 
+            print(e)
             traceback.print_exc()
 
 def server_chan_send(sckey, title, diffs):
@@ -44,10 +44,21 @@ def server_chan_send(sckey, title, diffs):
             req = requests.get(url)
             print("Server Chan send: {}".format(req.status_code))
         except Exception as e:
-            print(e) 
+            print(e)
             traceback.print_exc()
 
-
+def pushbullet_send(access_token, device_iden, title, diffs):
+    for diff in diffs:
+        url = 'https://api.pushbullet.com/v2/pushes'
+        payload = {'title': title, 'body': str(diff), 'url': diff.url, 'type': 'note',
+                'device_iden': device_iden}
+        headers = {'Access-Token': access_token}
+        try:
+            req = requests.post(url, data=payload, headers=headers)
+            print("PushBullet send: {}".format(req.status_code))
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
 
 def notify(diffs):
     if not len(diffs):
@@ -60,11 +71,24 @@ def notify(diffs):
     if config.get('server_chan', {}).get('SCKEY', None):
         server_chan_send(config['server_chan']['SCKEY'], title, diffs)
 
+    if os.environ.get('PUSHBULLET_ACCESS_TOKEN') and os.environ.get('PUSHBULLET_DEVICE_IDEN'):
+        pushbullet_send(
+                os.environ.get('PUSHBULLET_ACCESS_TOKEN'),
+                os.environ.get('PUSHBULLET_DEVICE_IDEN'),
+                title,
+                diffs)
+    elif config.get('pushbullet', {}).get('access_token') and config['pushbullet'].get('device_iden'):
+        pushbullet_send(
+                config['pushbullet']['access_token'],
+                config['pushbullet']['device_iden'],
+                title,
+                diffs)
+
     if os.environ.get('IFTTT_EVENT') and os.environ.get('IFTTT_KEY'):
         ifttt_send(os.environ.get('IFTTT_EVENT'), os.environ.get('IFTTT_KEY'), title, diffs)
     elif config.get('ifttt', {}).get('key') and config['ifttt'].get('event'):
         ifttt_send(config['ifttt']['event'], config['ifttt']['key'], title, diffs)
-    
+
     if os.environ.get('SENDGRID_API_KEY') and os.environ.get('SENDGRID_TO'):
         sendgrid_send(os.environ.get('SENDGRID_API_KEY'), os.environ.get('SENDGRID_TO'), title, diffs)
     elif config.get('sendgrid', {}).get('API_KEY') and config.get('sendgrid', {}).get('to'):
